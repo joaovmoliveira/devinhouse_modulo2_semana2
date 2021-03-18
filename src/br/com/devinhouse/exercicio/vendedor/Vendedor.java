7ccaab43025780b4a632e2fdd83c836436050b1b
João Victor Mendes de Oliveira <joao.m.oliveira@sesisc.org.br>
2021-03-17 23:52:12 -0300

    atualizadas classes do exercicio do Vendedor

--- src/br/com/devinhouse/exercicio/vendedor/Vendedor.java
+++ src/br/com/devinhouse/exercicio/vendedor/Vendedor.java
@@ -1,42 +1,56 @@
 package br.com.devinhouse.exercicio.vendedor;
 
 public class Vendedor {
-	private String nome;
-	private String sobreNome;
-	private double valorVendidoNoMes;
-	protected double salarioBase;
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public String getSobreNome() {
-		return sobreNome;
-	}
-	
-	public void setSobreNome(String sobreNome) {
-		this.sobreNome = sobreNome;
-	}
-	
-	public double getValorVendidoNoMes() {
-		return valorVendidoNoMes;
-	}
-	
-	public void setValorVendidoNoMes(double valorVendidoNoMes) {
-		this.valorVendidoNoMes = valorVendidoNoMes;
-	}
-	
-	public double getSalarioBase() {
-		return salarioBase;
-	}
-	
-	public void setSalarioBase(double salarioBase) {
-		this.salarioBase = salarioBase;
-	}
-	
-	
+    private String nome;
+    private String sobrenome;
+    private double valorVendidoNoMes;
+    private double salarioBase;
+    public Vendedor () {
+    }
+    public Vendedor (String nome, String sobrenome, double salarioBase) {
+        if (nome == null || nome.isBlank()) {
+            this.nome = "Não informado";
+        }
+        if (sobrenome == null || sobrenome.isBlank()) {
+            this.sobrenome = "Não informado";
+        }
+        if (salarioBase < 0) {
+            this.salarioBase = 0;
+        }
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.salarioBase = salarioBase;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getSobreNome() {
+        return sobrenome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public double getValorVendidoNoMes() {
+        return valorVendidoNoMes;
+    }
+    public void setValorVendidoNoMes(double valorVendidoNoMes) {
+        this.valorVendidoNoMes = valorVendidoNoMes;
+    }
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+    public double getSalario() {
+        return salarioBase;
+    }
+    @Override
+    public String toString () {
+        return String.format("O vendedor se chama %s %s, tem um salário de %.2f" +
+                " e vendeu neste mês %.2f", nome, sobrenome, getSalario(), valorVendidoNoMes);
+    }
 }
--- src/br/com/devinhouse/exercicio/vendedor/VendedorComissionado.java
+++ src/br/com/devinhouse/exercicio/vendedor/VendedorComissionado.java
@@ -1,20 +1,32 @@
 package br.com.devinhouse.exercicio.vendedor;
 
 public class VendedorComissionado extends Vendedor{
-	private double taxaDeComissao;
-	public double getTaxaDeComissao() {
-		return taxaDeComissao;
-	}
-	public boolean setTaxaDeComissao(double taxaDeComissao) {
-		if (taxaDeComissao < 0 || taxaDeComissao > 1) {
-			return false;
-		}
-		this.taxaDeComissao = taxaDeComissao;
-		return true;
-		
-	}
-	
-	public double getSalarioComissionado() {
-		return getSalarioBase() + (getTaxaDeComissao() * getValorVendidoNoMes());
-	}
+    private double taxaDeComissao;
+    public VendedorComissionado () {
+    }
+    public VendedorComissionado (String nome, String sobrenome, double salarioBase, double taxaDeComissao) {
+        super(nome, sobrenome, salarioBase);
+        if (taxaDeComissao < 0 || taxaDeComissao > 1) {
+            this.taxaDeComissao = 0;
+        }
+        this.taxaDeComissao = taxaDeComissao;
+    }
+    @Override
+    public double getSalario() {
+        return (getSalarioBase() + (this.taxaDeComissao * this.getValorVendidoNoMes()));
+    }
+    public double getTaxaDeComissao() {
+        return taxaDeComissao;
+    }
+    public boolean setTaxaDeComissao(double taxaDeComissao) {
+        if (taxaDeComissao < 0 || taxaDeComissao > 1) {
+            return false;
+        }
+        this.taxaDeComissao = taxaDeComissao;
+        return true;
+    }
+    @Override
+    public String toString () {
+        return (super.toString() + " e é vendedor comissionado, com uma taxa de comissão de " + this.taxaDeComissao);
+    }
 }
